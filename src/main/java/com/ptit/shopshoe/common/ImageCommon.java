70fd236fb09cd6f53985bf8b89672fea02fb6420
ngocquang <ngocquangptit139@gmail.com>
2021-05-25 00:47:19 +0700

    init send image

---
+++ src/main/java/com/ptit/shopshoe/common/ImageCommon.java
@@ -0,0 +1,9 @@
+package com.ptit.shopshoe.common;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageCommon {
+    public String sendImage(MultipartFile multipartFile){
+      return null;
+    }
+}
